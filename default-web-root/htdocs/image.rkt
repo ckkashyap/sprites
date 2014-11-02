244523114b06b9d3692655398f2f2c986f012552
CK Kashyap <ckkashyap@gmail.com>
2014-11-02 16:16:45 +0530

    working version

--- default-web-root/htdocs/image.rkt
+++ default-web-root/htdocs/image.rkt
@@ -1,52 +1,40 @@
-#lang slideshow
+#lang racket
 
 (require racket/draw)
 
+(define sprite-width 100)
+(define sprite-height 100)
+(define sprites-in-a-row 4)
+(define sprites-in-a-col 4)
 
-(define (my-spline x1 y1)
-  (dc (lambda (dc x y)
-        (send dc draw-spline
-              x y
-              (+ x 20) (+ y 80)
-              (+ x 100) (+ y 100)))
-      x1 y1))
+(define bitmap-width (* sprites-in-a-col sprite-width))
+(define bitmap-height (* sprites-in-a-row sprite-height))
 
+(define target (make-bitmap bitmap-width bitmap-height)) 
+(define image-dc (new bitmap-dc% [bitmap target]))
 
-(define pict (my-spline 100 200))
-
-
-;(define target (make-bitmap 400 200)) ; A 30x30 bitmap
-;(define image-dc (new bitmap-dc% [bitmap target]))
-
-(define (my-image x1 y1)
-(dc (lambda (image-dc x y)
       
 (send image-dc set-brush "green" 'transparent)
 (send image-dc erase)
 
-(for ( (row '(0 1 2 3))) 
-  (for ( (col '(0 1 2 3)))
+(for ( (row sprites-in-a-row)) 
+  (for ( (col sprites-in-a-col))
     (send image-dc draw-rectangle
-          (* col 100) (* row 100)
-          100 100) 
-    (let ( (rot-val (* 0.5 (+ col (* row 4))))
-           (tran-x (+ 30 (* col 100)))
-           (tran-y (+ 50 (* row 100)))
+          (* col sprite-width) (* row sprite-height)
+          sprite-width sprite-height) 
+    (let ( (rot-val (* -0.4 (+ col (* row 4))))
+           (tran-x (+ 45 (* col sprite-width)))
+           (tran-y (+ 50 (* row sprite-height)))
+	   (current-transformation (send image-dc get-transformation))
            )
       
       (send image-dc translate tran-x tran-y)
       (send image-dc rotate  rot-val)
-      (send image-dc draw-text "world" -20 0)
-      (send image-dc rotate  (* -1 rot-val))
-      (send image-dc translate (* -1 tran-x) (* -1 tran-y))
+      (send image-dc draw-text "Hello" -15 -5)
+      (send image-dc set-transformation current-transformation)
       
       )
     )
   )
       
-      ) x1 y1))
-
-(define aaa (my-image 400 400))
-      
-;(send target save-file "/home/ckk/Desktop/box.png" 'png)
-aaa
+(send target save-file "box.png" 'png)
